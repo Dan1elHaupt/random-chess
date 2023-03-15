610b31185f9917ab1f6eca0bd32f51f866efa02d
Daniel Haupt <danielh@bbd.co.za>
2023-03-15 08:35:26 +0200

    spring project

---
+++ src/test/java/com/gradprogram/randomchess/RandomChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradprogram.randomchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomChessApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
