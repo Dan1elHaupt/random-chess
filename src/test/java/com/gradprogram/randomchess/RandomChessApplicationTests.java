9729f1e0520e28b75157a97550ec12d5064a882d
Oarabile Moima <oarabile@bbd.co.za>
2023-03-20 14:45:25 +0200

    Dependencies done

--- src/test/java/com/gradprogram/randomchess/RandomChessApplicationTests.java
+++ src/test/java/com/gradprogram/randomchess/RandomChessApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RandomChessApplicationTests {
+public class RandomChessApplicationTests {
 
   @Test
   void contextLoads() {
