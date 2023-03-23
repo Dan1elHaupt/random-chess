771964d6ede2ba952d25a09531d1ffb409058dd4
Oarabile Moima <oarabile@bbd.co.za>
2023-03-23 09:07:49 +0200

    New way to input

--- src/main/java/com/gradprogram/randomchess/client/RunGame.java
+++ src/main/java/com/gradprogram/randomchess/client/RunGame.java
@@ -1,19 +1,66 @@
 package com.gradprogram.randomchess.client;
 
 import com.gradprogram.randomchess.model.GameStatus;
+import com.gradprogram.randomchess.model.board.Move;
 import com.gradprogram.randomchess.model.board.Point;
+import com.gradprogram.randomchess.model.piece.Piece;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RunGame {
 
   private static int[] coordinateConverter(String input) {
     char[] letterCoordinates = input.toCharArray();
 
-    int[] numberCoordinates = {letterCoordinates[0] - 97, Character.getNumericValue(letterCoordinates[1]) - 1,
-      letterCoordinates[3] - 97, Character.getNumericValue(letterCoordinates[4]) - 1};
+    int[] numberCoordinates = {letterCoordinates[0] - 97, Character.getNumericValue(letterCoordinates[1]) - 1};
 
     return numberCoordinates;
   };
+
+  public static boolean validInput(String input) {
+    input = input.toLowerCase();
+    if (input.length() == 2) {
+      String letter1 = String.valueOf(input.charAt(0));
+      Pattern pattern = Pattern.compile("^[a-h]+$");
+      Matcher matcher = pattern.matcher(letter1);
+      boolean digit = Character.isDigit(input.charAt(1));
+      return (matcher.matches() && digit);
+    }
+    return false;
+  }
+
+  public static Point startingPoint(Game game) {
+    List<Piece> currentPieces;
+    if( game.isWhiteToPlay() ) {
+      currentPieces = game.getBoard().whitePieces;
+    }
+    else {
+      currentPieces = game.getBoard().blackPieces;
+    }
+    Collections.shuffle(currentPieces);
+
+    System.out.println(currentPieces.get(0));
+
+    for (Piece currentPiece : currentPieces) {
+      for (int x = 0; x < 8; x++) {
+        for (int y = 0; x < 8; x++) {
+          Move previousMove =  game.getMoves().size() != 0 ? game.getMoves().get(game.getMoves().size() - 1) : null;
+          if (game.getBoard().isLegalMove(new Point(currentPiece.getX(),currentPiece.getY()), new Point(x,y), game.isWhiteToPlay(), previousMove)) {
+            System.out.println("HERE");
+            return new Point(x,y);
+          }
+        }
+      }
+    }
+
+
+    return null;
+  }
 
   public static void startGame() {
     Point start, end;
@@ -25,7 +72,12 @@ public class RunGame {
     game.getBoard().printBoard();
 
     while (game.getGameStatus() == GameStatus.ACTIVE) {
+      start = startingPoint(game);
       String input = scanner.nextLine();
+      if (!validInput(input)) {
+        System.out.println("Please enter a valid block. Letter followed by a number, eg C2");
+        continue;
+      }
 
       if (input.equals("resign")) {
         if (game.isWhiteToPlay()) {
@@ -38,8 +90,7 @@ public class RunGame {
 
       points = coordinateConverter(input);
 
-      start = new Point(points[0], points[1]);
-      end = new Point(points[2], points[3]);
+      end = new Point(points[0], points[1]);
       game.makeMove(start, end);
 
       game.getBoard().printBoard();
--- src/test/java/com/gradprogram/randomchess/RandomChessApplicationTests.java
+++ src/test/java/com/gradprogram/randomchess/RandomChessApplicationTests.java
@@ -1,13 +1,61 @@
 package com.gradprogram.randomchess;
 
+import com.gradprogram.randomchess.client.RunGame;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class RandomChessApplicationTests {
+public class RandomChessApplicationTests {
 
   @Test
-  void contextLoads() {
+  void input1() {
+    String input = "X1";
+    boolean valid = RunGame.validInput(input);
+    assertEquals(false, valid);
+  }
+
+  @Test
+  void input2() {
+    String input = "a1";
+    boolean valid = RunGame.validInput(input);
+    assertEquals(true, valid);
+  }
+
+  @Test
+  void input3() {
+    String input = "G5";
+    boolean valid = RunGame.validInput(input);
+    assertEquals(true, valid);
+  }
+
+  @Test
+  void input4() {
+    String input = "H8";
+    boolean valid = RunGame.validInput(input);
+    assertEquals(true, valid);
+  }
+
+  @Test
+  void input5() {
+    String input = "i7";
+    boolean valid = RunGame.validInput(input);
+    assertEquals(false, valid);
+  }
+
+  @Test
+  void input6() {
+    String input = "H87";
+    boolean valid = RunGame.validInput(input);
+    assertEquals(false, valid);
+  }
+
+  @Test
+  void input7() {
+    String input = "7a";
+    boolean valid = RunGame.validInput(input);
+    assertEquals(false, valid);
   }
 
 }
