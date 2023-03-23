a419ff823bdf9205b6d50b6db46f3a3cf6e2899d
Gabriel Tidy <gabriel@bbd.co.za>
2023-03-23 11:23:01 +0200

    added validation for input

--- src/main/java/com/gradprogram/randomchess/client/RunGame.java
+++ src/main/java/com/gradprogram/randomchess/client/RunGame.java
@@ -3,11 +3,30 @@ package com.gradprogram.randomchess.client;
 import com.gradprogram.randomchess.model.GameStatus;
 import com.gradprogram.randomchess.model.board.Point;
 import java.util.Scanner;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class RunGame {
 
+  private static boolean inputValidator(String input) {
+     if (input.equals("resign")) {
+      return true;
+     }
+     if (input.length() != 5) {
+      return false;
+     }
+
+    char[] letterCoordinates = input.toCharArray();
+    if ((int) letterCoordinates[0] < 97 || (int) letterCoordinates[0] > 104 || (int) letterCoordinates[3] < 97 || (int) letterCoordinates[3] > 104) {
+      return false;
+    }
+    if ((int) letterCoordinates[1] < 49 || (int) letterCoordinates[1] > 56 || (int) letterCoordinates[4] < 49 || (int) letterCoordinates[4] > 56) {
+      return false;
+    }
+    return true;
+  }
+
   private static int[] coordinateConverter(String input) {
-    //TODO: add check for correct format
     char[] letterCoordinates = input.toCharArray();
 
     int[] numberCoordinates = {letterCoordinates[0] - 97, Character.getNumericValue(letterCoordinates[1]) - 1,
@@ -27,6 +46,11 @@ public class RunGame {
 
     while (game.getGameStatus() == GameStatus.ACTIVE) {
       String input = scanner.nextLine();
+
+      while (!inputValidator(input)) {
+        log.info("Input must be of the form: e2 e4 (Moves a piece from square e2 to square e4)");
+        input = scanner.nextLine();
+      }
 
       if (input.equals("resign")) {
         if (game.isWhiteToPlay()) {
